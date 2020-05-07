3e4b6dd442607e7f252fa21d7551f3b4075ec08a
Laith Alquran <lmalquran@gmail.com>
2020-05-07 02:47:31 -0700

    Changed forecast data

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ weatherForm.addEventListener('submit', (e) => {
             }
 
             messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast.temperature
+            messageTwo.textContent = data.forecast
         })
     })
 })
