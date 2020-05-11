70cf51eebb89540a43c69761db09a0ffe70e5eda
Laith Alquran <lmalquran@gmail.com>
2020-05-11 13:35:23 -0700

    added constants folder"

---
+++ src/utils/geocode.js
@@ -0,0 +1,32 @@
+const request = require('request')
+const keys = require('../constants/keys')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + keys.GEOCODE_API 
+    request({url, json: true}, (err, { body } = {}) => {
+        if (err) {
+            callback('Unable to connect to location services', undefined)
+            return
+        }
+
+        if (body.features.length === 0) {
+            callback('Unable to find location', undefined)
+            return
+        }
+
+        const location = body.features[0]
+        const longitude = location.center[0]
+        const latitude = location.center[1]
+
+        callback(undefined, {
+            latitude,
+            longitude,
+            location: location.text
+        })
+    })
+
+}
+
+module.exports = {
+    geocode
+}
